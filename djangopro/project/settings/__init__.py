c49143af739a9b8d5c85956aa5b8f98f5bfb41d4
kskibun <costam@costam.pl>
2024-12-17 19:02:54 +0100

    all project

--- djangopro/project/settings/__init__.py
+++ djangopro/project/settings/__init__.py
@@ -15,4 +15,4 @@ if not LOCAL_SETTINGS_PATH:
 if not os.path.isabs(LOCAL_SETTINGS_PATH):
     LOCAL_SETTINGS_PATH = str(BASE_DIR / LOCAL_SETTINGS_PATH)
 
-include('base.py', 'custom.py', optional(LOCAL_SETTINGS_PATH), 'envvars.py', 'docker.py')
+include('base.py', 'logging.py', 'custom.py', optional(LOCAL_SETTINGS_PATH), 'envvars.py', 'docker.py')
